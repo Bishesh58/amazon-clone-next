04663eb8869735b3807b552e67c81cadb2bfa583
Bishesh <bishesh.sunam@gmail.com>
2022-07-20 02:10:54 +1200

    added page route

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,9 +5,11 @@ import {
   ShoppingCartIcon,
 } from "@heroicons/react/outline";
 import { signIn, signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
 function Navbar() {
   const { data: session } = useSession();
+  const router = useRouter();
 
   return (
     <header>
@@ -15,6 +17,7 @@ function Navbar() {
       <div className="bg-amazon_blue p-1 py-2 flex items-center flex-grow">
         <div className="mt-2 flex flex-grow items-center sm:flex-grow-0">
           <Image
+            onClick={() => router.push("/")}
             src="https://links.papareact.com/f90"
             width={150}
             height={40}
@@ -29,11 +32,8 @@ function Navbar() {
           />
           <SearchIcon className="h-12 p-4" />
         </div>
-        <div
-          onClick={!session ? signIn : signOut}
-          className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap"
-        >
-          <div className=" link">
+        <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
+          <div onClick={!session ? signIn : signOut} className=" link">
             <p>{session ? `Hello, ${session.user.name}!` : "Sign In"}</p>
             <p className="font-extrabold md:text-sm">Account & Lists</p>
           </div>
@@ -41,7 +41,10 @@ function Navbar() {
             <p>Returns</p>
             <p className="font-extrabold md:text-sm">& Orders</p>
           </div>
-          <div className="link relative flex items-center">
+          <div
+            onClick={() => router.push("/checkout")}
+            className="link relative flex items-center"
+          >
             <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">
               0
             </span>
---
+++ pages/checkout.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+function checkout() {
+  return (
+
+    <div>
+       <Navbar />
+      <div>
+        this is checkout page.
+      </div>
+    </div>
+  )
+}
+
+export default checkout
