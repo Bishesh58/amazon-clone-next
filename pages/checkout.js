5fc93d5a1862564711e5089cdd5b8a7a00745bba
Bishesh <bishesh.sunam@gmail.com>
2022-07-21 23:53:33 +1200

    added stripe config

--- pages/checkout.js
+++ pages/checkout.js
@@ -4,10 +4,36 @@ import Navbar from "../components/Navbar";
 import { useSelector } from "react-redux";
 import { selectItems, selectTotal } from "../slices/basketSlice";
 import CheckoutProduct from "../components/CheckoutProduct";
+import { useSession } from "next-auth/react";
+import { loadStripe } from "@stripe/stripe-js";
+import axios from "axios";
+
+const stripePromise = loadStripe(process.env.stripe_public_key);
 
 function checkout() {
   const items = useSelector(selectItems);
   const total = useSelector(selectTotal);
+  const { data: session } = useSession();
+
+  const createCheckoutSession = async () => {
+    //getting stripe.js instace
+    const stripe = await stripePromise;
+
+    //call backend to create the checkout Session
+    const checkoutSession = await axios.post("/api/create-checkout-session", {
+      items,
+      email: session.user.email,
+    });
+
+    //redirect user to checkout
+    const result = await stripe.redirectToCheckout({
+      sessionId: checkoutSession.data.id,
+    });
+
+    if (result.error) {
+      alert(result.error.message);
+    }
+  };
 
   return (
     <div className="bg-gray-100">
@@ -43,6 +69,28 @@ function checkout() {
           </div>
         </div>
         {/* Right */}
+        <div className="flex flex-col bg-white shadow-sm p-10">
+          {items.length > 0 && (
+            <>
+              <h2 className="whitespace-nowrap">
+                Subtotal ({items.length} items):{" "}
+                <span className="font-bold">${total}</span>
+              </h2>
+              <button
+                disabled={!session}
+                onClick={createCheckoutSession}
+                role="link"
+                s
+                className={`button mt-2 ${
+                  !session &&
+                  "from-gray-300 to-gray-500 border-gray-200 text-gray-300 cursor-not-allowed"
+                }`}
+              >
+                {!session ? "Sign In to Checkout" : "Proceed to Checkout"}
+              </button>
+            </>
+          )}
+        </div>
       </main>
     </div>
   );
